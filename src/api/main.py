2680d01483a87cd3d3e3c2fd360579aae1224734
Daniel Lavía <danilavia.art@gmail.com>
2026-05-18 15:44:30 +0200

    update/main and schema

--- src/api/main.py
+++ src/api/main.py
@@ -7,30 +7,12 @@ from fastapi import Path as FastApiPath
 from pathlib import Path as FilePath
 from contextlib import asynccontextmanager
 from pydantic import BaseModel, Field
-
+from .schemas import PredictInput  #con esto llamamos a schemas.py para usar los modelos de datos que definimos allí, como PredictInput para la predicción. Evitamos así tener un main.py gigante y desordenado, y mantenemos la estructura modular y limpia.
 # Configuración de rutas
 BASE_DIR = FilePath(__file__).resolve().parent.parent.parent
 RUTA_CSV = BASE_DIR / "data" / "processed" / "data_idname.csv" # direccion del csv procesado
 RUTA_MODELO = BASE_DIR / "models" / "modelo_entrenado.pkl" # direccion del pickle del modelo entrenado
 
-class PredictInput(BaseModel):
-    Administrative: int = Field(..., description="Número de páginas administrativas visitadas", example=0)
-    Administrative_Duration: float = Field(..., description="Tiempo total en páginas administrativas", example=0.0)
-    Informational: int = Field(..., description="Número de páginas informativas visitadas", example=0)
-    Informational_Duration: float = Field(..., description="Tiempo total en páginas informativas", example=0.0)
-    ProductRelated: int = Field(..., description="Número de páginas relacionadas con productos visitadas", example=1)
-    ProductRelated_Duration: float = Field(..., description="Tiempo total en páginas de productos", example=11.25)
-    BounceRates: float = Field(..., description="Porcentaje de rebote de la página", example=0.0)
-    ExitRates: float = Field(..., description="Porcentaje de salida de la página", example=0.1)
-    PageValues: float = Field(..., description="Valor medio de la página web", example=0.0)
-    SpecialDay: float = Field(..., description="Cercanía a una fecha especial o festivo (0.0 a 1.0)", example=0.0)
-    Month: str = Field(..., description="Mes de la sesión (ej: Feb, Mar, May, Nov)", example="May")
-    OperatingSystems: int = Field(..., description="ID del sistema operativo del usuario", example=2)
-    Browser: int = Field(..., description="ID del navegador utilizado", example=2)
-    Region: int = Field(..., description="ID de la región del usuario", example=1)
-    TrafficType: int = Field(..., description="ID del tipo de tráfico de origen", example=1)
-    VisitorType: str = Field(..., description="Tipo de visitante: Returning_Visitor, New_Visitor, Other", example="Returning_Visitor")
-    Weekend: bool = Field(..., description="Indica si la sesión ocurrió en fin de semana", example=True)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -40,6 +22,7 @@ async def lifespan(app: FastAPI):
         # Guardamos el modelo directamente en el state de la app
         app.state.modelo = joblib.load(RUTA_MODELO)
         print("¡Modelo optimizado cargado con éxito!")
+        modelocargado = True
     except FileNotFoundError:
         print("Error: El archivo del modelo no existe.")
         raise RuntimeError("Archivo de modelo no encontrado.")
@@ -64,9 +47,11 @@ app = FastAPI(
 @app.get("/")
 def inicio():
     return {
-        "status": "API en ejecución. Prueba",
+        "status": "API en ejecución /health para check de salud",
         "documentacion": "/docs",
-        "mensaje": "Visita /docs para probar el endpoint de predicción."
+        "query revenue": "http://127.0.0.1:8000/datos/revenue?valor=true",
+        "query cliente": "http://127.0.0.1:8000/datos/450",
+        "Check": "http://127.0.0.1:8000/health",
     }
 
 @app.get("/health")
---
+++ src/api/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from pydantic import Field
+
+class PredictInput(BaseModel):
+    Administrative: int = Field(..., description="Número de páginas administrativas visitadas", example=0)
+    Administrative_Duration: float = Field(..., description="Tiempo total en páginas administrativas", example=0.0)
+    Informational: int = Field(..., description="Número de páginas informativas visitadas", example=0)
+    Informational_Duration: float = Field(..., description="Tiempo total en páginas informativas", example=0.0)
+    ProductRelated: int = Field(..., description="Número de páginas relacionadas con productos visitadas", example=1)
+    ProductRelated_Duration: float = Field(..., description="Tiempo total en páginas de productos", example=11.25)
+    BounceRates: float = Field(..., description="Porcentaje de rebote de la página", example=0.0)
+    ExitRates: float = Field(..., description="Porcentaje de salida de la página", example=0.1)
+    PageValues: float = Field(..., description="Valor medio de la página web", example=0.0)
+    SpecialDay: float = Field(..., description="Cercanía a una fecha especial o festivo (0.0 a 1.0)", example=0.0)
+    Month: str = Field(..., description="Mes de la sesión (ej: Feb, Mar, May, Nov)", example="May")
+    OperatingSystems: int = Field(..., description="ID del sistema operativo del usuario", example=2)
+    Browser: int = Field(..., description="ID del navegador utilizado", example=2)
+    Region: int = Field(..., description="ID de la región del usuario", example=1)
+    TrafficType: int = Field(..., description="ID del tipo de tráfico de origen", example=1)
+    VisitorType: str = Field(..., description="Tipo de visitante: Returning_Visitor, New_Visitor, Other", example="Returning_Visitor")
+    Weekend: bool = Field(..., description="Indica si la sesión ocurrió en fin de semana", example=True)
